1c2239a7585390d9af65106f745de1013e015efb
Shaun Williams <shaunewilliams@gmail.com>
2011-07-25 03:44:07 -0500

    updated tetra globe with rotation and projection logic (not working yet)

--- globes/tetra.lua
+++ globes/tetra.lua
@@ -18,7 +18,8 @@ d = s/2/tan(theta) -- center to edge
 e = r*cos(d60) -- face to edge
 f = h-c -- center to face
 
-fov = 2*atan(s/2/f)
+fovr = 2*atan(s/2/f)
+fovd = fovr * 180 / pi
 
 local y,z
 y = e - e*e/(r+e)
@@ -28,36 +29,52 @@ plates = {
    {
       {0,y/e,z/e},
       {0,(e-y)/e,(-f-z)/e}, 
-      fov
+      fovd
    },
    {
       {-y/e*sin(d120),y/e*cos(d120),z/e},
       {-(e-y)/e*sin(d120),(e-y)/e*cos(d120),(-f-z)/e}, 
-      fov
+      fovd
    },
    {
       {-y/e*sin(-d120),y/e*cos(-d120),z/e},
       {-(e-y)/e*sin(-d120),(e-y)/e*cos(-d120),(-f-z)/e}, 
-      fov
+      fovd
    },
-   {{0,0,-1},{0,1,0},fov}
+   {{0,0,-1},{0,1,0},fovd}
 }
 
 function ray_to_plate(x,y,z)
 
    -- determine the plate (plate forward vector closest to ray)
-   local plate = 0
+   local plate = 1
    local mina = tau
    local i
-   for i in 1,4 do
+   for i=1,4 do
       -- note: we are assuming the ray (x,y,z) is normalized. but is it?
-      local a = abs(acos(x*plate[i][1][1]+y*plate[i][1][2]+z*plate[i][1][3]))
+      local a = abs(acos(x*plates[i][1][1]+y*plates[i][1][2]+z*plates[i][1][3]))
       if a < mina then
          mina = a
-         plate = i-1
+         plate = i
       end
    end
 
-   -- TODO: rotate ray such that the plate's forward is (0,0,1) and up is (0,1,0)
-   --       (may require quaternion rotation)
+   -- rotate ray such that the plate's forward is (0,0,1) and up is (0,1,0)
+   local u = plates[plate][2]
+   local f = plates[plate][1]
+   local r = {
+      u[2]*f[3]-u[3]*f[2],
+      u[1]*f[3]-u[3]*f[1],
+      u[1]*f[2]-u[2]*f[1]
+   }
+   local nx = r[1]*x + r[2]*y + r[3]*z
+   local ny = u[1]*x + u[2]*y + u[3]*z
+   local nz = f[1]*x + f[2]*y + f[3]*z
+
+   -- get texture coordinates
+   local dist = 0.5 / tan(fovr/2)
+   local u = nx/nz*dist+0.5
+   local v = ny/nz*dist+0.5
+
+   return plate-1, u, v
 end
