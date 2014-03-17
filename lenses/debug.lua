9f0fa6570912bf78cd927ce7f9443539d016f680
Shaun Williams <shaunewilliams@gmail.com>
2014-03-16 20:59:45 -0500

    made debug lens display plate table with 2 rows

--- lenses/debug.lua
+++ lenses/debug.lua
@@ -1,15 +1,27 @@
-cols = numplates
-rows = 1
+if numplates == 4 then
+    rows = 2
+    cols = {2,2}
+elseif numplates == 5 then
+    rows = 2
+    cols = {3,2}
+elseif numplates == 6 then
+    rows = 2
+    cols = {3,3}
+else
+    rows = 1
+    cols = {numplates}
+end
+maxcols = math.max(unpack(cols))
 
-lens_width = cols
+lens_width = maxcols
 lens_height = rows
 
 onload = "f_contain"
 
-function col(x)
-   local nx = x+cols/2
+function col(rowcols,x)
+   local nx = x+rowcols/2
    local i,f = math.modf(nx)
-   if nx < 0 or nx >= cols then
+   if nx < 0 or nx >= rowcols then
       return nil, nil
    end
    return i,f
@@ -26,11 +38,20 @@ end
 
 function lens_inverse(x,y)
    local r,v = row(y)
-   local c,u = col(x)
-   if r == nil or c == nil then
+   if r == nil then
       return nil 
+   end
+
+   local c,u = col(cols[r+1],x)
+   if c == nil then
+      return nil
    else
-      local plate = r*cols+c
+      local plate = c
+      local i = 0
+      while i < r do
+         plate = plate + cols[i+1]
+         i = i + 1
+      end
       return plate_to_ray(plate,u,v)
    end
 end
