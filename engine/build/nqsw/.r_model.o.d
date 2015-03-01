2549792016df4674fc243c73aa4f5ea4f55770f7
Shaun Williams <shaunewilliams@gmail.com>
2015-03-01 12:24:16 -0600

    ignore build files from engine directory

--- engine/build/nqsw/.r_model.o.d
+++
@@ -1,2 +0,0 @@
-build/nqsw/r_model.o: common/r_model.c
-common/r_model.c :
--- engine/build/nqsw/.rb_tree.o.d
+++
@@ -1,4 +0,0 @@
-build/nqsw/rb_tree.o: common/rb_tree.c \
-  /Users/Swilliams/code/blinky/include/rb_tree.h
-common/rb_tree.c :
-  /Users/Swilliams/code/blinky/include/rb_tree.h :
