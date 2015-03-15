4823c59988be34381380a82dea24fee92935f134
Shaun Williams <shaunewilliams@gmail.com>
2015-03-15 06:23:01 -0500

    start quincuncial inverse (display front and back hemispheres side-by-side)

--- game/lua-scripts/lenses/quincuncial.lua
+++ game/lua-scripts/lenses/quincuncial.lua
@@ -1,126 +1,120 @@
-size = 2.622058
-lens_width = 2*size
-lens_height = 2*size
+eps = 0.0001
+halfpi = pi/2
 
+--------------------------------------------------------------------
+-- Matlab's Jacobi Elliptic function:
+--    returns [sn, cn, dn, ph](u|m).
+-- implemntation from d3-geo-projection:
+--   (at https://github.com/d3/d3-geo-projection/blob/26b0147156534b3e09f402a2628d0fe209d33f8b/src/elliptic.js#L26-L74)
+function asqrt(x)
+  if x > 0 then
+    return sqrt(x)
+  end
+  return 0
+end
+function ellipj(u, m)
+  local ai, b, phi, t, twon
+  if (m < eps) then
+    t = sin(u)
+    b = cos(u)
+    ai = .25 * m * (u - t * b)
+    return t - ai * b,
+      b + ai * t,
+      1 - .5 * m * t * t,
+      u - ai
+  end
+  if (m >= 1 - eps) then
+    ai = .25 * (1 - m)
+    b = cosh(u)
+    t = tanh(u)
+    phi = 1 / b
+    twon = b * sinh(u)
+    return t + ai * (twon - u) / (b * b),
+      phi - ai * t * phi * (twon - u),
+      phi + ai * t * phi * (twon + u),
+      2 * atan(exp(u)) - halfpi + ai * (twon - u) / b
+  end
+
+  local a = {1, 0, 0, 0, 0, 0, 0, 0, 0}
+  local c = {sqrt(m), 0, 0, 0, 0, 0, 0, 0, 0}
+  local i = 1
+  b = sqrt(1 - m)
+  twon = 1
+
+  while (abs(c[i] / a[i]) > eps and i < 9) do
+    ai = a[i]
+    i = i+1
+    c[i] = .5 * (ai - b)
+    a[i] = .5 * (ai + b)
+    b = asqrt(ai * b)
+    twon = twon*2
+  end
+
+  phi = twon * a[i] * u
+  repeat
+    b = phi
+    t = c[i] * sin(b) / a[i]
+    phi = .5 * (asin(t) + phi)
+    i = i-1
+  until (i == 1)
+
+  t = cos(phi)
+  return sin(phi), t, t / cos(phi - b), phi
+end
+
+--------------------------------------------------------------------
+-- from Appendix A of "Warping Pierce Quincuncial Panoramas"
+--    by Chamberlain Fong, Brian Vogel
+
+sqrt2 = sqrt(2)
+sqrt22 = sqrt2/2
+m = 1/2
+ke = 1.85407467730137
+
+function cnrectify(x,y)
+  -- mapping of square coordinates to spherical coordinates
+  -- input: x,y in the normalized square with corners at (+-1,+-1)
+  -- output: latitude, longitude coordinates to fetch in a sphere
+  local xpr = ke*(sqrt22*x-sqrt22*y)/sqrt2+ke
+  local ypr = ke*(sqrt22*x+sqrt22*y)/sqrt2
+  local sni,cni,dni
+  local x1,y1
+  local phi,psi
+  local s,c,d
+  local s1,c1,d1
+  local delta
+  if abs(ypr)<eps then
+    sni,cni,dni = ellipj(xpr,m)
+    x1 = cni
+    y1 = 0.0
+  else
+    phi = xpr
+    psi = ypr
+    s,c,d = ellipj(phi,m)
+    s1,c1,d1 = ellipj(psi,1-m)
+    delta = c1^2 + m*s^2*s1^2
+    x1=(c*c1)/delta
+    y1=-(s*d*s1*d1)/delta
+  end
+  -- stereographic projection equations
+  longd = atan2(y1,x1)
+  latp = 2*atan2(sqrt(x1*x1+y1*y1),1)-halfpi
+  return latp, longd
+end
+
+lens_height = 2
+lens_width = 4
 onload = "f_contain"
 
-C = { 2.19174570831038, 0.0914203033408211, -0.00575574836830288, -0.0012804644680613, 5.30394739921063e-05, 3.12960480765314e-05, 2.02692115653689e-07, -8.58691003636495e-07}
+function lens_inverse(x,y)
+  if abs(x) > 2 or abs(y) > 1 then
+    return nil
+  end
+  x = x+1
+  local lat,lon = cnrectify(x,y)
+  local x0,y0,z0 = latlon_to_ray(lat,-lon)
 
-ORDER = 8
-RSQRT2 = 0.7071067811865475244008443620
-
-function ell_int_5(phi)
-   local d1 = 0
-   local d2 = 0
-   local y,y2,temp
-   local Cp = C[ORDER]
-   
-   y = phi * 2/pi
-   y = 2*y*y-1
-   y2 = 2*y
-
-   for i=ORDER-1,1,-1 do
-      d1,d2 = y2*d1-d2+Cp, d1
-      Cp = C[i]
-   end
-
-   return phi*(y*d1-d2+Cp/2)
-end
-
-function proj_asin(v)
-   local av = abs(v)
-   if av >= 1 then
-      if v < 0 then
-         return -pi/2
-      else
-         return pi/2
-      end
-   end
-   return asin(v)
-end
-
-function proj_acos(v)
-   local av = abs(v)
-   if av >= 1 then
-      if v < 0 then
-         return pi
-      else
-         return 0
-      end
-   end
-   return acos(v)
-end
-
-function lens_forward(x,y,z)
-   local lat,lon = ray_to_latlon(x,y,z)
-   local m,n
-   local a = 0
-   local b = 0
-   local sm = false
-   local sn = false
-   local back = false
-
-   local sp = sin(lat)
-   if (abs(lon) - 0.0000001) > pi then
-      return nil
-   end
-   if (abs(lon) - 0.0000001) > pi/2 then
-      back = true
-      if lon > 0 then
-         lon = lon - pi
-      else
-         lon = lon + pi
-      end
-   end
-
-   if (abs(lon) - 0.0000001) > pi/2 then
-      return nil
-   end
-
-   a = cos(lat) * sin(lon)
-   if sp + a < 0 then
-      sm = true
-   end
-   if sp - a < 0 then
-      sn = true
-   end
-   a = proj_acos(a)
-   b = pi/2 - lat
-
-   m = proj_asin(sqrt(abs(1+cos(a+b))))
-   if sm then
-      m = -m
-   end
-   n = proj_asin(sqrt(abs(1-cos(a-b))))
-   if sn then
-      n = -n
-   end
-   local x = ell_int_5(m)
-   local y = ell_int_5(n)
-
-   -- rotate by 45 degrees
-   local temp = x
-   x = RSQRT2 * (x-y)
-   y = RSQRT2 * (temp+y)
-
-   if back then
-      x = -x
-      if x*y > 0 then
-         x,y = -y,-x
-      else
-         x,y = y,x
-      end
-      if x > 0 then
-         x = x - lens_width/2
-      else
-         x = x + lens_width/2
-      end
-      if y > 0 then
-         y = y - lens_height/2
-      else
-         y = y + lens_height/2
-      end
-   else
-   end
-   return x,y
+  -- rotate from south pole to origin
+  local x1,y1,z1 = x0, z0, -y0
+  return x1,y1,z1
 end
